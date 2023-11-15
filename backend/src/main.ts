3d206453ed2dce85f4496636a5c199c88bb165b3
Abaulderstone <alex.baulderstone@gmail.com>
2023-11-16 00:53:45 +1100

    sets up SSR

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(','),
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'PATCH',
+      'DELETE',
+      'HEAD',
+      'CONNECT',
+      'OPTIONS',
+      'TRACE',
+    ],
+    maxAge: 600,
+  });
   await app.listen(8080);
   console.log('Running!');
 }
