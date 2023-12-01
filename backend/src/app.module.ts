8ad9c17daea8e012cb864bbeaefdc417ea5674ce
Abaulderstone <alex.baulderstone@gmail.com>
2023-12-01 16:02:50 +1030

    adds bull config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { PostModule } from './post/post.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), PostModule],
+  imports: [
+    MikroOrmModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST ?? 'cache',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({ name: 'emailSending' }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
