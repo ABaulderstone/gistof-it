9161e68ca14c4dc248e8ccd521be380f492e59e9
Abaulderstone <alex.baulderstone@gmail.com>
2023-12-01 21:26:09 +1030

    updates to use config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,25 +6,35 @@ import { PostModule } from './post/post.module';
 import { BullModule } from '@nestjs/bull';
 import { EmailModule } from './email/email.module';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     MikroOrmModule.forRoot(),
-    BullModule.forRoot({
-      redis: {
-        host: process.env.REDIS_HOST ?? 'cache',
-        port: 6379,
-      },
-    }),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.EMAIL_HOST,
-        port: 587,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        redis: {
+          host: configService.get<string>('REDIS_HOST', 'cache'),
+          port: configService.get<number>('REDIS_PORT', 6379),
         },
-      },
+      }),
+      inject: [ConfigService],
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('EMAIL_HOST'),
+          port: configService.get<number>('EMAIL_PORT', 587),
+          auth: {
+            user: configService.get<string>('EMAIL_USERNAME'),
+            pass: configService.get<string>('EMAIL_PASSWORD'),
+          },
+        },
+      }),
+      inject: [ConfigService],
     }),
     PostModule,
     EmailModule,
